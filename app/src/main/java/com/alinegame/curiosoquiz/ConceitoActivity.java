8e5c5bf2d167359607898918184fabcc850427dc
Aline.Gonz <laingonzaga@hotmail.com>
2018-03-11 23:18:49 -0300

    Adiciona arquivo

---
+++ app/src/main/java/com/alinegame/curiosoquiz/ConceitoActivity.java
@@ -0,0 +1,13 @@
+package com.alinegame.curiosoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConceitoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conceito);
+    }
+}
